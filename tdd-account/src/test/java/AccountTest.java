24ffb002bf6b0f297be34c0d548d1ce9f8ba2918
DWL <DWL>
2020-12-11 01:25:01 +0900

    feat: 입출금 기능 추가

--- tdd-account/src/test/java/AccountTest.java
+++ tdd-account/src/test/java/AccountTest.java
@@ -4,10 +4,10 @@ import static org.junit.Assert.fail;
 import org.junit.Test;
 
 public class AccountTest {
-
+    Account account = null;
     @Test
     public void testAccount() throws Exception {
-        Account account = new Account(10000);
+        account = new Account(10000);
         if (account == null) {
             throw new Exception("계좌생성 실패");
        }
@@ -15,8 +15,22 @@ public class AccountTest {
 
     @Test
     public void testGetBalance() throws Exception {
-        Account account = new Account(10000);
+        account = new Account(10000);
         assertEquals(10000, account.getBalance());
     }
 
+
+    @Test
+    public void testDeposit() throws Exception {
+        account = new Account(10000);
+        account.deposit(1000);
+        assertEquals(11000, account.getBalance());
+    }
+
+    @Test
+    public void testWithdraw() throws Exception {
+        account = new Account(10000);
+        account.withdraw(1000);
+        assertEquals(9000, account.getBalance());
+    }
 }
