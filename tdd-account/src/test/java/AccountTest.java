d7d7d79cd0142ea86ebf70f036d2947d989be109
DWL <DWL>
2020-12-11 01:26:00 +0900

    refactor: account 생성 => setup 함수 추출

--- tdd-account/src/test/java/AccountTest.java
+++ tdd-account/src/test/java/AccountTest.java
@@ -5,9 +5,14 @@ import org.junit.Test;
 
 public class AccountTest {
     Account account = null;
+
+    private void setUp() {
+        account = new Account(10000);
+    }
+
     @Test
     public void testAccount() throws Exception {
-        account = new Account(10000);
+        setUp();
         if (account == null) {
             throw new Exception("계좌생성 실패");
        }
@@ -15,21 +20,21 @@ public class AccountTest {
 
     @Test
     public void testGetBalance() throws Exception {
-        account = new Account(10000);
+        setUp();
         assertEquals(10000, account.getBalance());
     }
 
 
     @Test
     public void testDeposit() throws Exception {
-        account = new Account(10000);
+        setUp();
         account.deposit(1000);
         assertEquals(11000, account.getBalance());
     }
 
     @Test
     public void testWithdraw() throws Exception {
-        account = new Account(10000);
+        setUp();
         account.withdraw(1000);
         assertEquals(9000, account.getBalance());
     }
